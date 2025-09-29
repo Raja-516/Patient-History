dabe3fd86eaff883164d5a3bb9a23d4c0fb74a87
Raja-516 <akularaja44@gmail.com>
2025-09-29 14:48:16 +0530

    Login /Signup Changed

--- my-app/components/Login.jsx
+++ my-app/components/Login.jsx
@@ -1,39 +1,62 @@
-
+import { useState } from "react";
 import "./mainn.css";
 
-
 function Login() {
-  
+  const [isLogin, setIsLogin] = useState(true);
 
   return (
     <div className="login-box">
       <div className="exp">
-        <h2>Login</h2>
-        <form className="login-form" >
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Enter your name"
-            
-            required
-          />
+        <button
+          className={`design ${isLogin ? "active" : ""}`}
+          onClick={() => setIsLogin(true)}
+        >
+          Login
+        </button>
+        <button
+          className={`design ${!isLogin ? "active" : ""}`}
+          onClick={() => setIsLogin(false)}
+        >
+          Sign Up
+        </button>
+      
 
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter your password"
-            
-            required
-          />
+      <form className="login-form">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name"
+          required
+        />
 
-          
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+        />
 
-          <button type="submit" className="login-btn">Submit</button>
-        </form>
+        {!isLogin && (
+          <>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm your password"
+              required
+            />
+          </>
+        )}
+
+        <button type="submit" className="login-btn">
+          {isLogin ? "Login" : "Sign Up"}
+        </button>
+      </form>
       </div>
     </div>
   );
